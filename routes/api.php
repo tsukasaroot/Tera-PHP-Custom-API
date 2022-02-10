977044e5b1a09e984279d25f80436b9b9b3e9912
Jordan <jordan.rouco-miguez@epitech.eu>
2022-02-10 10:04:57 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,22 @@
 use Core\Routes;
 use Core\Http;
 
-Routes::get(route: '/', closure: function () {
-	Http::send_json([ 'message' => 'Welcome' ]);
-});
-
-Routes::post(route: '/', closure: function () {
-	Http::send_json([ 'message' => 'Welcome' ]);
-});
-
-Routes::post(route: '/welcome', action: 'WelcomeController@test_post');
+/**
+ * Client
+ */
+Routes::post('tera/LauncherLoginAction', 'Tera\\teraController@login');
+Routes::post('tera/GetAccountInfoByUserNo', 'Tera\\teraController@getAccountInfoByUserNo');
+Routes::post('authApi/GameAuthenticationLogin','authApi\\GameLoginController@login');
+Routes::post('api/GetUserInfo', 'Api\\getUserInfoController@info');
+Routes::post('api/EnterGame', 'Api\\UserServerInteraction@EnterGame');
+Routes::post('api/LeaveGame', 'Api\\UserServerInteraction@LeaveGame');
+/**
+ * Arb_GW
+ */
+Routes::get('api/ServiceTest', 'Arbiter\\ArbiterApi@ServiceTest');
+Routes::get('systemApi/RequestAPIServerStatusAvailable', 'Arbiter\\systemApi\\SystemApi@status');
+Routes::post('api/GetServerPermission', 'Arbiter\\ArbiterApi@GetServerPermission');
+Routes::post('api/ServerDown', 'Arbiter\\ArbiterApi@ServerDown');
+Routes::post('api/CreateChar', 'Arbiter\\ArbiterApi@CreateChar');
+Routes::post('api/DeleteChar', 'Arbiter\\ArbiterApi@ModifyChar');
+Routes::post('api/ModifyChar', 'Arbiter\\ArbiterApi@DeleteChar');
